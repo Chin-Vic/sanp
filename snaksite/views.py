70b3abaf5c97f4782a282dee9092de0b6710c20c
vic <chen.feng@ieee.org>
2023-01-23 05:08:59 +0800

    fininsh combine img

--- snaksite/views.py
+++ snaksite/views.py
@@ -1,12 +1,16 @@
+import base64
+import cv2
+import numpy as np
+import os
+import threading
+import time
+import win32print
+import win32ui
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
-from django.shortcuts import render
 from django.http import JsonResponse
-import time
-import os
-import base64
-import threading
-from PIL import Image
+from PIL import Image, ImageWin
+
 
 def is_ajax(request):
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
@@ -110,16 +114,68 @@ def print_pic(request):
 
 def img_post_process(imgs,img_type):
     img_postion_info = get_imgs_postion_info(img_type)
-    for img in imgs:
-        bmp = Image.open (os.path.join('./static/images', img))
+    background = Image.open(os.path.join('./static/assets/images', img_type+'.png'))
+    empty = Image.new('RGB', (background.size))
+    for idx,img in enumerate(imgs):
+        bmp = Image.open (os.path.join('./static/', img))
+        w_ = int(img_postion_info[idx][0])
+        h_ = int(img_postion_info[idx][1])
+        cx = int(img_postion_info[idx][2])
+        cy = int(img_postion_info[idx][3])
+        bmp = bmp.resize((w_,h_))
+        empty.paste(bmp,(int(cx-w_/2),int(cy-h_/2)))
+
+    pic = cv2.cvtColor(np.asarray(empty), cv2.COLOR_RGB2BGR)
+    pic = cv2.cvtColor(pic, cv2.COLOR_BGR2BGRA)
+    background = cv2.cvtColor(np.asarray(background), cv2.COLOR_RGB2BGR)
+    background = cv2.cvtColor(background, cv2.COLOR_BGR2BGRA) 
+    h = pic.shape[0]           
+    w = pic.shape[1]                  
+    for x in range(w):
+        for y in range(h):
+            r = background[y, x, 2]
+            g = background[y, x, 1]  
+            b = background[y, x, 0] 
+            if r==0 and g==0 and  b==0:
+                background[y, x] = pic[y, x]
+    cv2.imwrite('./static/image_fin.png', background)
 
 
-
-def print_photo(imgs):
-    pass 
+def print_photo():
+    HORZRES = 8
+    VERTRES = 10
+    PHYSICALWIDTH = 110
+    PHYSICALHEIGHT = 111
+    PHYSICALOFFSETX = 112
+    PHYSICALOFFSETY = 113
+    printer_name = win32print.GetDefaultPrinterW() # 獲得預設印表機
+    hDC = win32ui.CreateDC ()
+    hDC.CreatePrinterDC (printer_name)
+    printable_area = hDC.GetDeviceCaps (HORZRES), hDC.GetDeviceCaps (VERTRES)
+    printer_size = hDC.GetDeviceCaps (PHYSICALWIDTH), hDC.GetDeviceCaps (PHYSICALHEIGHT)
+    bmp = Image.open ('./static/image_fin.png')
+    if bmp.size[0] > bmp.size[1]:
+        bmp = bmp.rotate (90)
+    ratios = [1.0 * printable_area[0] / bmp.size[0], 1.0 * printable_area[1] / bmp.size[1]]
+    scale = min (ratios)
+    hDC.StartDoc ('test')
+    hDC.StartPage ()
+    dib = ImageWin.Dib (bmp)
+    scaled_width, scaled_height = [int (scale * i) for i in bmp.size]
+    x1 = int ((printer_size[0] - scaled_width) / 2)
+    y1 = int ((printer_size[1] - scaled_height) / 2)
+    x2 = x1 + scaled_width
+    y2 = y1 + scaled_height
+    dib.draw (hDC.GetHandleOutput (), (x1, y1, x2, y2))
+    hDC.EndPage ()
+    hDC.EndDoc ()
+    hDC.DeleteDC ()
+ 
 
 def get_imgs_postion_info(img_type):
+    position = []
     f = open(os.path.join('./static/position', img_type+'.txt'))
     for line in f.readlines():
-        print(line)
+        position.append(line.split(','))
     f.close
+    return position
